4a6f4c015199d9fec24b06b1b7060947251cddc5
Gonzalo Torres Arroyo <torres.argon25@triana.salesianos.edu>
2026-01-15 10:34:13 +0100

    chore: initial version

---
+++ prueba/src/ejercicio/MiniCalc.java
@@ -0,0 +1,13 @@
+package ejercicio;
+
+public class MiniCalc {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		public static int add(int a, int b) { return a + b; }
+		
+		
+	}
+
+}
